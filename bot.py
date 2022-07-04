9dad2f27d5b5d33f070e178830e639dbe8a59980
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2022-07-04 17:24:49 +0800

    Added PyCharm stuff

--- bot.py
+++ bot.py
@@ -1,3 +1,8 @@
+"""
+The Crimson Paw Moderation Bot
+
+Written in Python by Kerby Keith Aquino<skepfoosky15@gmail.com>
+"""
 import disnake
 import os
 import argparse
@@ -6,11 +11,13 @@ from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 
+
 class CrimsonPaw(disnake.Client):
-      async def on_ready(self):
-        print(f"Logged on as {self.user}")
-        
-      async def on_message(self, message):
-          print(f"Message from {message.author}: \"{message.content}\"")
+    async def on_ready(self):
+        print(f"{Fore.LIGHTGREEN_EX}Client is ready! Logged on as {self.user}.{Style.RESET_ALL}")
+
+    async def on_message(self, message):
+        print(f"Message from {message.author}: \"{message.content}\"")
+
 
 client = CrimsonPaw().run(os.getenv("TOKEN"))
