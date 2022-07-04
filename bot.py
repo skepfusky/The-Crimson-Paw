f0c6ae3fe887a56829d471be4cb18b1aa14cf0da
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2022-07-04 18:23:30 +0800

    nice

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 
 intents = disnake.Intents.all()
-client = commands.Bot(command_prefix="d!", intents=intents, sync_commands_debug=True)
+client = commands.Bot(intents=intents, sync_commands=True)
 
 @client.slash_command(description="Ping the bot for testing purposes")
 async def ping(inter):
@@ -20,7 +20,7 @@ async def ping(inter):
 
 @client.event
 async def on_ready():
-    print(f"{Back.GREEN}[!]{Back.RESET} Bot is ready!")
+    print(f"{Back.GREEN}[!]{Back.RESET} Bot is ready! Logged in as {client.user}")
     # await client.change_presence(activity=disnake.Game(name="testing testing"))
 
 client.run(os.getenv("TOKEN"))
