499a44b560b3cd7d1893d848326acedb65ca5369
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2022-07-04 21:09:48 +0800

    embed test baby

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 """
-The Crimson Paw Moderation Bot
+The Crimson Paw - A Moderation Bot for The Bad Guy's Server
 
-Written in Python by Kerby Keith Aquino<skepfoosky15@gmail.com>
+By Kerby Keith Aquino <skepfoosky15@gmail.com>
 """
 import disnake
 import os
@@ -11,16 +11,20 @@ from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 
-intents = disnake.Intents.all()
-client = commands.Bot(intents=intents, sync_commands=True)
+client = commands.Bot(command_prefix="d!", intents=disnake.Intents.all(), sync_commands=True)
 
 @client.slash_command(description="Ping the bot for testing purposes")
 async def ping(inter):
     await inter.response.send_message("Bing bongs")
 
+@client.slash_command(description="Another embed test command")
+async def yeet(ctx):
+    embed = disnake.Embed(title="Pong!", description="Bing bongs", color=0x00ff00)
+    await ctx.send(embed=embed)
+
 @client.event
 async def on_ready():
-    print(f"{Back.GREEN}[!]{Back.RESET} Bot is ready! Logged in as {client.user}")
-    # await client.change_presence(activity=disnake.Game(name="testing testing"))
+    print(f"{Fore.GREEN}[!]{Fore.RESET} Bot is ready! Logged in as {client.user}")
+    await client.change_presence(activity=disnake.Game(name="as the Governor"))
 
 client.run(os.getenv("TOKEN"))
