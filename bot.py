977cfbd9464ac52330336f3bbd7862f4d90e90f2
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2022-07-19 03:30:40 +0800

    Added one command

--- bot.py
+++ bot.py
@@ -5,21 +5,36 @@ By Kerby Keith Aquino <skepfoosky15@gmail.com>
 """
 import disnake
 import os
+from dotenv import load_dotenv
 from disnake.ext import commands, tasks
 from colorama import *
-from dotenv import load_dotenv, find_dotenv
 
-load_dotenv(find_dotenv())
+load_dotenv()
 
-client = commands.Bot(command_prefix="d!", intents=disnake.Intents.all(), sync_commands=True)
+client = commands.Bot(intents=disnake.Intents.all(), sync_commands=True)
 
+# Main execution
 @client.slash_command(description="Ping the bot for testing purposes")
 async def ping(inter):
     await inter.response.send_message("Bing bongs")
 
 @client.slash_command(description="Another embed test command")
-async def yeet(ctx):
-    embed = disnake.Embed(title="Pong!", description="Bing bongs", color=0x00ff00)
+async def help(ctx):
+    embed = disnake.Embed(title="Pong!", description="mark is bae", color=0x00ff00)
+    embed.set_image(url="https://media.giphy.com/media/ycTVuMYzKu4vHg1OLG/giphy.gif")
+    await ctx.send(embed=embed)
+
+@client.slash_command(description="Get user information")
+async def user(ctx, user: str):
+    if user == "":
+        user = ctx.author.name
+
+    user = await client.get_user(user)
+    embed = disnake.Embed(title=f"{user.name}#{user.discriminator}", color=0x00ff00)
+    embed.set_thumbnail(url=user.avatar_url)
+    embed.add_field(name="ID", value=user.id, inline=False)
+    embed.add_field(name="Status", value=user.status, inline=False)
+    embed.add_field(name="Created At", value=user.created_at, inline=False)
     await ctx.send(embed=embed)
 
 @client.event
